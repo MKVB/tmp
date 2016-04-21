3597ceb4fbf3c7db63bd4abd975f0f7f41eae7b3
Vasil Balezdrov <VasilBalezdrov@users.noreply.github.com>
2016-04-21 22:47:00 +0300

    add

--- Stack/Stack.h
+++ Stack/Stack.h
@@ -1,22 +1,23 @@
-#pragma once
 #ifndef STACK_H
 #define STACK_H
-
+using namespace std;
 
 template <typename T>
 class Stack
 {
 public:
-	Stack<T>(int n );
+	Stack<T>(int n);
 	~Stack<T>();
-	Stack<T>(const &Stack<T> other);
-	Stack<T> operator=(const &Stack<T> other);
-    void push(T x);
+	Stack<T>(const Stack<T>& other);
+	Stack<T>& operator=(const Stack<T>& other);
+	void push(T x);
 	void pop();
-	T top() const;
-	bool IsEmpty();
+	T top1() const;
+	bool IsEmpty1();
+	void print() const;
+	void resize();
 private:
-    T*stack;
+	T*stack;
 	int capacity;
 	int top;
 };
@@ -28,66 +29,110 @@ private:
 #include <ctime>
 
 template <typename T>
-Stack<T>::Stack<T>(int n)
+Stack<T>::Stack(int n)
 {
-	this->capacity=n;
-	stack=new T[n];
-	top=0;
+	this->capacity = n;
+	stack = new T[n];
+	top = 0;
 
 }
 template <typename T>
-Stack<T>::~Stack<T>()
+Stack<T>::~Stack()
 {
-	delete [] stack;
+	delete[] stack;
 
 }
 template <typename T>
-Stack<T>::Stack<T>(const &Stack<T> other)
+Stack<T>::Stack(const Stack<T>& other)
 {
-	this->capacity=other.capacity;
-	this->top=other.top;
-	for	(int i=0,int<other.top,i++)
+	this->capacity = other.capacity;
+	this->top = other.top;
+	this->stack = new T[other.capacity];
+	for (int i = 0; i < other.top; i++)
 	{
-		stack[i]=other.stack[i];
+		stack[i] = other.stack[i];
 	}
-
 }
 template <typename T>
-Stack<T>::Stack<T> operator=(const &Stack<T> other)
+Stack<T>& Stack<T>:: operator=(const Stack<T>& other)
 {
-	if(this!=&other)
+	if (this != &other)
 	{
-		delete [] stack;
-		this->capacity=other.capacity;
-	this->top=other.top;
-	for	(int i=0,int<other.top,i++)
-	{
-		stack[i]=other.stack[i];
-	}
+		delete[] stack;
+		this->capacity = other.capacity;
+		this->top = other.top;
+		this->stack = new T[other.capacity];
+		for (int i = 0; i < other.top; i++)
+		{
+			stack[i] = other.stack[i];
+		}
 	}
 	return *this;
 }
 
 template <typename T>
-Stack<T>::void push(T x)
+void Stack<T>::push(T x)
 {
-	stack[i]=x;
+	if ((top) == capacity)
+	{
+		resize();
+	}
+	stack[top] = x;
 	top++;
 }
 template <typename T>
-	Stack<T>::void pop()
-	{
-		top--;
-	}
-template <typename T>
-T Stack<T>::top() const
+void Stack<T>::pop()
 {
-	return stack[top];
-
+	top--;
+}
+template <typename T>
+T Stack<T>::top1() const
+{
+	return stack[top - 1];
 }
 
 template <typename T>
-Stack<T>::bool IsEmpty()
+bool Stack<T>::IsEmpty1()
 {
-	return top==0;
+	return top == 0;
+}
+template <typename T>
+void Stack<T>::print() const
+{
+	for (int i = top - 1; i >= 0; i--)
+	{
+		cout << stack[i] << endl;
+	}
+}
+template <typename T>
+void Stack<T>::resize()
+{
+	capacity *= 2;
+	T*	stack1 = new T[capacity];
+	for (int i = 0; i < this->top; i++)
+	{
+		stack1[i] = stack[i];
+	}
+	delete[] stack;
+	stack = stack1;
+}
+
+
+int main()
+{
+	Stack<int> a(2);
+	a.push(1);
+	a.push(2);
+	a.push(3);
+	a.push(4);
+	a.push(5);
+	a.pop();
+	cout << a.top1() << endl;
+	//a.print();
+	////a.print();
+	//a.pop();
+	//cout << a.top1() << endl;
+	//cout <<boolalpha<< a.IsEmpty1() << endl;
+	system("pause");
+	return 0;
 }
